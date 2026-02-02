7fe862b0990c65c12da23ad981d4a7f0fad6f5f5
Theogene <igiranezatheogene4@gmail.com>
2026-02-02 12:20:01 +0200

    advanced rest parameter

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
-function getTotal() {
-    var numbers = [];
+function combine() {
+    var args = [];
     for (var _i = 0; _i < arguments.length; _i++) {
-        numbers[_i] = arguments[_i];
+        args[_i] = arguments[_i];
     }
-    return numbers.reduce(function (sum, val) { return sum + val; }, 0);
+    var total = 0;
+    var str = '';
+    args.forEach(function (arg) {
+        if (typeof arg === 'number') {
+            total += arg;
+        }
+        else if (typeof arg === 'string') {
+            str += arg;
+        }
+    });
+    return [total, str];
 }
-console.log(getTotal()); // 0
-console.log(getTotal(10, 20)); // 30
-console.log(getTotal(10, 20, 30)); // 60
+var _a = combine(3, 'Happy', 2, 1, ' New Year'), total = _a[0], str = _a[1];
+console.log({ total: total });
+console.log({ str: str });
