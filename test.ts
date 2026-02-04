c6d42d7a380bd5f80bf8d4e0a3db3b5404ae2879
Theogene <igiranezatheogene4@gmail.com>
2026-02-04 10:25:59 +0200

    interface

--- test.ts
+++ test.ts
@@ -1,41 +1,14 @@
-abstract class Employee {
-  constructor(private firstName: string, private lastName: string) {}
-  abstract getSalary(): number;
-  get fullName(): string {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  compensationStatement(): string {
-    return `${this.fullName} makes ${this.getSalary()} a month.`;
-  }
+interface Person {
+  firstName: string
+  lastName: string
 }
 
-// let employee = new Employee('John','Doe'); => Error
-
-class FullTimeEmployee extends Employee {
-    constructor(firstName: string, lastName: string, private salary: number) {
-        super(firstName, lastName);
-    }
-    getSalary(): number {
-        return this.salary;
-    }
+function fullName(person: Person) {
+  return `${person.firstName} ${person.lastName}`
 }
 
-class Contractor extends Employee {
-  constructor(
-    firstName: string,
-    lastName: string,
-    private rate: number,
-    private hours: number
-  ) {
-    super(firstName, lastName);
-  }
-  getSalary(): number {
-    return this.rate * this.hours;
-  }
+let emp = {
+  firstName: 'Igiraneza',
+  lastName: 'Theogene'
 }
-
-let john = new FullTimeEmployee('John', 'Doe', 12000);
-let jane = new Contractor('Jane', 'Doe', 100, 160);
-
-console.log(john.compensationStatement());
-console.log(jane.compensationStatement());
+console.log(fullName(emp))
